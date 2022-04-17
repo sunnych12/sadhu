41e7faa6472f6f35e31dcc91b75cd8c8df6f2629
sunnych12 <102491116+sunnych12@users.noreply.github.com>
2022-04-17 14:30:06 +0530

    Update sadhu.py

--- sadhu.py
+++ sadhu.py
@@ -1,3 +1,4 @@
 print("welcome to the world using jenkins")
 print("qwerty is the machince")
 print("all oceans are blue and all fishes are white")
+print("all the people who are in the team was working in the situation to the person of the world")
